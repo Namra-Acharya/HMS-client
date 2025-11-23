c1d2d19c1fa352dafe971f23c54aad628b77758a
Namra Acharya <namraacharya112@gmail.com>
2025-11-23 18:12:25 +0530

    Updated project

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import offlineDB from './offlineDB';
 
-const API_BASE_URL = '/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
